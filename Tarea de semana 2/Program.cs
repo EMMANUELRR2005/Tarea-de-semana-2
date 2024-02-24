f61b89f04438ab3d6a8c291722f4a67f925b39eb
Emmanuel Rodríguez <emmanuel15072005@outlook.com>
2024-02-24 07:01:32 -0600

    15 version

--- Tarea de semana 2/Program.cs
+++ Tarea de semana 2/Program.cs
@@ -2,20 +2,12 @@
 using System.Transactions;
 using System.Diagnostics.CodeAnalysis;
 
-//Escribe un programa que lea una cadena de texto y busque una palabra en ella
+//Escribe un programa que lea una cadena de texto y cuente el numero de vocales que tiene 
 
 Console.WriteLine("Escriba el texto");
 string texto = Console.ReadLine();
-Console.WriteLine("Cual es la palabra que busca?");
-string palabra = Console.ReadLine();
-if (texto.Contains(palabra))
-
-
-    Console.WriteLine("La palabra si está en el texto");
-
-else
-
-    Console.WriteLine("La palabra no esta en el texto");
+int vocales = Regex.Matches(texto, "[aeiouAEIOU]").Count;
+Console.WriteLine("Su palabra tiene: " + vocales);
 
 
 
