468d800ef2a4ba71f5de51eacae239bc6e1b7e37
Emmanuel Rodríguez <emmanuel15072005@outlook.com>
2024-02-24 06:00:10 -0600

    Primera version

--- Tarea de semana 2/Program.cs
+++ Tarea de semana 2/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System.Text.RegularExpressions;
+using System.Transactions;
+
+//Codigo que convierte un numero entero a decimal
+
+int entero = 10;
+
+decimal deci = Convert.ToDecimal(entero);
+
+Console.WriteLine("El numero 10 en decimal es: " + deci.ToString("0.0"));
