76b5e6fd5eea2255e669d54b7c6c22b57b29d725
Emmanuel Rodríguez <emmanuel15072005@outlook.com>
2024-02-24 07:04:16 -0600

    Version 16

--- Tarea de semana 2/Program.cs
+++ Tarea de semana 2/Program.cs
@@ -2,14 +2,15 @@
 using System.Transactions;
 using System.Diagnostics.CodeAnalysis;
 
-//Escribe un programa que lea una cadena de texto y cuente el numero de vocales que tiene 
+//Escriba un programa que calcule el area de un triangulo 
 
-Console.WriteLine("Escriba el texto");
-string texto = Console.ReadLine();
-int vocales = Regex.Matches(texto, "[aeiouAEIOU]").Count;
-Console.WriteLine("Su palabra tiene: " + vocales);
+Console.WriteLine("Ingrese la base del triangulo");
+int bases = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la altura del triangulo");
+int altura = int.Parse(Console.ReadLine());
 
-
+float area = (bases * altura) / 2f;
+Console.WriteLine("el area del triangulo es: " + area);
 
 
 
